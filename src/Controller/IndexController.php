473a486045f776d570942ee9a9412829cdc2a5dc
Lucas Delobelle <lucas@ld-web.net>
2022-11-16 06:44:31 +0100

    :sparkles: add controllers + twig templates

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -8,13 +8,30 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
 {
-    /**
-     * @Route("/", name="app_index")
-     */
-    public function index(): Response
-    {
-        return $this->render('index/index.html.twig', [
-            'controller_name' => 'IndexController',
-        ]);
-    }
+  /**
+   * @Route("/", name="app_promo")
+   */
+  public function promo(): Response
+  {
+    return $this->render('index/promo.html.twig');
+  }
+
+  /**
+   * @Route("/home", name="app_index")
+   */
+  public function index(): Response
+  {
+    return $this->render('index/index.html.twig', [
+      'controller_name' => 'IndexController',
+      'username' => 'Bobby'
+    ]);
+  }
+
+  /**
+   * @Route("/contact", name="app_contact")
+   */
+  public function contact(): Response
+  {
+    return $this->render('index/contact.html.twig');
+  }
 }
